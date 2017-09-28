9a5032f245ac99b33c999313611e66a9f997ec2d
Dustin <dyelland@gmail.com>
2017-09-28 09:20:22 -0500

    changed webpack for routes

--- src/components/noteItem/index.js
+++ src/components/noteItem/index.js
@@ -4,10 +4,21 @@ import ReactDOM from 'react-dom';
 class NoteItem extends React.Component {
   constructor(props){
     super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidUpdate(){
     console.log('Update Item State',this.props.App.state);
+  }
+
+  handleClick(event){
+    this.props.App.setState(state => {
+      return ({
+        notes: state.notes.filter(note => {
+          //return note.id !== /*note id*/;
+        })
+      })
+    })
   }
 
   render(){
@@ -15,7 +26,7 @@ class NoteItem extends React.Component {
       <li key={this.props.note.id}>
         {this.props.note.content}
 
-        <button>Delete</button>
+        <button onClick={this.handleClick}>Delete</button>
       </li>
     )
   }
